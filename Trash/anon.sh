b91d8a95b8e4c4685ed1e622e16163892584ff08
Cuttlerat <avkioller@gmail.com>
2018-08-11 23:18:19 +0300

    Add anon.sh

---
+++ Trash/anon.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+[[ "${TRACE:-}" ]] && set -x
+
+
+readonly API="!!!!!!!CHANGE_ME!!!!!!!"
+readonly CHAT_ID="!!!!!!!CHANGE_ME!!!!!!!"
+readonly URL="https://api.telegram.org/bot${API}"
+readonly TIMEOUT=10
+
+send_message() {
+    local TEXT=$( sed 's/^\s\+//g' <<< "${1}" )
+    curl -s "${URL}/sendMessage"                                              \
+            --max-time "${TIMEOUT}"                                           \
+            --output /dev/null                                                \
+            --data-urlencode "text=${TEXT}"                                   \
+                      --data "chat_id=${CHAT_ID}"                             
+}
+
+offset() {
+    local UPDATE_ID="$(( $1 + 1 ))"
+    curl -s "${URL}/getUpdates" \
+         --data "offset=${UPDATE_ID}" \
+         -o /dev/null
+}
+
+main() {
+
+    while raw_message=$(curl -s "${URL}/getUpdates"); do
+        msg_count=$(jq '.result | length' <<<"${raw_message}")
+
+        for i in $( seq 0 "${msg_count:=-1}"); do
+            if [[ $(jq -r ".result[${i}].message.chat.type" <<<"${raw_message}") != "private" ]]; then
+                continue
+            fi
+            UPDATE_ID=$(jq -r ".result[${i}].update_id" <<<"${raw_message}")
+            TEXT=$(jq -r ".result[${i}].message.text" <<<"${raw_message}")
+            send_message "${TEXT}"
+            offset "${UPDATE_ID}"
+        done
+    done
+
+}
+
+main "$@"
